9cd8954043329314674374f7fd8a9f1fd6577b95
giftsunni <giftsunday34@gmail.com>
2025-06-05 04:29:53 -0400

    Add Dynamic Pages

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -1,5 +1,5 @@
 # Uncomment the required imports before adding the code
-
+from .restapis import get_request, analyze_review_sentiments, post_review
 from .models import CarMake, CarModel
 from django.shortcuts import render
 from django.http import HttpResponseRedirect, HttpResponse
